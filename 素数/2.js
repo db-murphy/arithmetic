a74067389a66be9c49a18771229817676203dc6d
黄小龙 <huangxiaolong@huangxiaolongdeMacBook-Pro.local>
2018-10-31 17:49:09 +0800

    update

--- 素数/2.js
+++ 素数/2.js
@@ -6,12 +6,13 @@ function isPrime(n){
      return true;
   if (n % 2 == 0)
      return false;
+
   while (divisor <= limit)
   {
     if (n % divisor == 0)
       return false;
     else
-      divisor += 2;
+      divisor += 2; // 加2的原因是因为在前面除2的时候已经被过滤了，既然能被偶数整除，那肯定能被2整除
   }
   return true;
 }
